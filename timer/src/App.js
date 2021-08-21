7120f432a7cdeb19e60f4c68437c439d6b9018b6
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-08-21 16:13:13 -0300

    Finish project Timer

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      number: 0,
+      btnText: "Start"
+    };
+
+    this.timer = null;
+    this.startTimer = this.startTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
+  }
+
+  startTimer(){
+    let state = this.state;
+
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null;
+      state.btnText = "Start";
+    }else{
+      this.timer = setInterval(() => {
+        state.number += 0.1;
+        this.setState(state);
+      }, 100);
+      state.btnText = "Stop";
+    }
+
+    this.setState(state);
+  }
+
+  resetTimer(){
+    let state = this.state;
+
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+
+    state.number = 0;
+    state.btnText = "Start";
+    this.setState(state);
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <h3 className="title">Timer Online</h3>
+        <img className="img-timer" src={ require('./cronometro.png').default } />
+        <div class="timer-number">
+          <span>{ this.state.number.toFixed(1) }</span>
+        </div>
+        <div className="btn-container">
+          <button onClick={ this.startTimer } className="btn">{ this.state.btnText }</button>
+          <button onClick={ this.resetTimer } className="btn">Reset</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
